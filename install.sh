a93345682da4ee8da9478b1e33f5856bb2cf2188
LiinxTV <64651327+LiinxTV@users.noreply.github.com>
2022-02-09 11:37:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ request() {
 
 request "CREATE DATABASE grafana;"
 request "DROP TABLE IF EXISTS data;"
-request "CREATE TABLE data (ip varchar(15) NOT NULL,country varchar(48) NOT NULL,city varchar(48) NOT NULL,zip varchar(12) NOT NULL,lat decimal(10,8) NOT NULL,lng decimal(11,8) NOT NULL,isp varchar(48) NOT NULL,time date NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
+request "USE grafana; CREATE TABLE data (ip varchar(15) NOT NULL,country varchar(48) NOT NULL,city varchar(48) NOT NULL,zip varchar(12) NOT NULL,lat decimal(10,8) NOT NULL,lng decimal(11,8) NOT NULL,isp varchar(48) NOT NULL,time date NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
 
 if [ $(dpkg-query -W -f='${Status}' xmlstarlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
