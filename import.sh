dacfa4b6e8812caf4a30dd86fe6fadffdcf9ed1e
LiinxTV <64651327+LiinxTV@users.noreply.github.com>
2022-02-09 11:19:53 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-user="root"
-password="P@ssw0rd"
-database="grafana"
+user=$( xmlstarlet sel -t -v '/config/user' "/etc/fail2ban-monitoring/config.xml" )
+password=$( xmlstarlet sel -t -v '/config/password' "/etc/fail2ban-monitoring/config.xml" )
+database=$( xmlstarlet sel -t -v '/config/database' "/etc/fail2ban-monitoring/config.xml" )
 
 request() {
 	mysql -u$user -p$password --database=$database -e "$1"
