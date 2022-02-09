ef1e6212cb396a49164f0c69e8b5d5f7966adc2b
LiinxTV <64651327+LiinxTV@users.noreply.github.com>
2022-02-09 09:45:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!bin/bash
 
+user=$( xmlstarlet sel -t -v '/config/user' "/etc/fail2ban-monitoring/config.xml" )
+password=$( xmlstarlet sel -t -v '/config/password' "/etc/fail2ban-monitoring/config.xml" )
+database=$( xmlstarlet sel -t -v '/config/database' "/etc/fail2ban-monitoring/config.xml" )
+
+request() {
+	mysql -u$user -p$password -e "$1"
+}
+
+request "CREATE DATABASE grafana;"
+request "DROP TABLE IF EXISTS `data`;
+request "CREATE TABLE `data` (`ip` varchar(15) NOT NULL,`country` varchar(48) NOT NULL,`city` varchar(48) NOT NULL,`zip` varchar(12) NOT NULL,`lat` decimal(10,8) NOT NULL,`lng` decimal(11,8) NOT NULL,`isp` varchar(48) NOT NULL,`time` date NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
+
 sudo apt update && sudo apt upgrade -y
 
 apt install xmlstarlet -y
